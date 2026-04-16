a321cbbc1519d18163ab4e6c0f7e4985bbeae20c
jiaochuchoudoufu <3151411899@qq.com>
2026-04-16 19:16:40 +0800

    Updates

--- assets/HomePage-STOKk7Px.js
+++
@@ -1,1 +0,0 @@
-import{p as e,t,u as n,y as r}from"./index-8r-yd6UZ.js";var i={},a={class:`home`};function o(t,i){return r(),e(`div`,a,[...i[0]||=[n(`h1`,null,`🏠 首页`,-1),n(`p`,null,`欢迎使用 LifeLog AI`,-1)]])}var s=t(i,[[`render`,o],[`__scopeId`,`data-v-69228290`]]);export{s as default};
